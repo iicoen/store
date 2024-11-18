17d6cc4df3bc2f058cf22e2f169556c14d856dd0
Your Name <you@example.com>
2024-11-18 18:19:52 +0200

    אולי כלום

--- frontend/public/hostages-ticker-custom.js
+++ frontend/public/hostages-ticker-custom.js
@@ -1,4 +1,59 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const n of document.querySelectorAll('link[rel="modulepreload"]'))d(n);new MutationObserver(n=>{for(const o of n)if(o.type==="childList")for(const p of o.addedNodes)p.tagName==="LINK"&&p.rel==="modulepreload"&&d(p)}).observe(document,{childList:!0,subtree:!0});function c(n){const o={};return n.integrity&&(o.integrity=n.integrity),n.referrerPolicy&&(o.referrerPolicy=n.referrerPolicy),n.crossOrigin==="use-credentials"?o.credentials="include":n.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function d(n){if(n.ep)return;n.ep=!0;const o=c(n);fetch(n.href,o)}})();(function(){const l={he:{days:"ימים",hours:"שעות",minutes:"דקות",seconds:"שניות",subtitle1:"מאות",subtitle2:"חטופים",subtitle3:"על ידי החמאס",title1:"נלחמים בחמאס בכל הכח וככה מחזירים אותם",title2:"עכשיו"},en:{days:"days",hours:"hours",minutes:"minutes",seconds:"seconds",subtitle1:"HUNDREDS ARE HELD",subtitle2:"HOSTAGE",subtitle3:"BY HAMAS",title1:"BRING THEM HOME",title2:"NOW"}},r=`
+(function () {
+  const r = document.createElement("link").relList;
+  if (r && r.supports && r.supports("modulepreload")) return;
+  for (const n of document.querySelectorAll('link[rel="modulepreload"]')) d(n);
+  new MutationObserver((n) => {
+    for (const o of n)
+      if (o.type === "childList")
+        for (const p of o.addedNodes)
+          p.tagName === "LINK" && p.rel === "modulepreload" && d(p);
+  }).observe(document, { childList: !0, subtree: !0 });
+  function c(n) {
+    const o = {};
+    return (
+      n.integrity && (o.integrity = n.integrity),
+      n.referrerPolicy && (o.referrerPolicy = n.referrerPolicy),
+      n.crossOrigin === "use-credentials"
+        ? (o.credentials = "include")
+        : n.crossOrigin === "anonymous"
+        ? (o.credentials = "omit")
+        : (o.credentials = "same-origin"),
+      o
+    );
+  }
+  function d(n) {
+    if (n.ep) return;
+    n.ep = !0;
+    const o = c(n);
+    fetch(n.href, o);
+  }
+})();
+(function () {
+  const l = {
+      he: {
+        days: "ימים",
+        hours: "שעות",
+        minutes: "דקות",
+        seconds: "שניות",
+        subtitle1: "מאות",
+        subtitle2: "חטופים",
+        subtitle3: "על ידי החמאס",
+        title1: "נלחמים בחמאס בכל הכח וככה מחזירים אותם",
+        title2: "עכשיו",
+      },
+      en: {
+        days: "days",
+        hours: "hours",
+        minutes: "minutes",
+        seconds: "seconds",
+        subtitle1: "HUNDREDS ARE HELD",
+        subtitle2: "HOSTAGE",
+        subtitle3: "BY HAMAS",
+        title1: "BRING THEM HOME",
+        title2: "NOW",
+      },
+    },
+    r = `
     <svg xmlns="http://www.w3.org/2000/svg" width="125" height="23" viewBox="0 0 125 23" fill="none">
       <path d="M113.985 16.2096L112.343 1.97803H114.056L115.204 14.2705L116.37 1.97803H118.242L119.425 14.2705L120.521 1.97803H122.251L120.591 16.2096H118.207L117.288 6.14076L116.37 16.2096H113.985Z" fill="#E82900"/>
       <path d="M108.618 16.3875C106.976 16.3875 105.898 15.3379 105.898 13.6123V4.57526C105.898 2.84969 106.976 1.80011 108.618 1.80011C110.279 1.80011 111.356 2.84969 111.356 4.57526V13.6123C111.356 15.3379 110.279 16.3875 108.618 16.3875ZM108.618 14.7864C109.236 14.7864 109.643 14.395 109.643 13.6479V4.53969C109.643 3.79253 109.236 3.40116 108.618 3.40116C108.018 3.40116 107.611 3.79253 107.611 4.53969V13.6479C107.611 14.395 108.018 14.7864 108.618 14.7864Z" fill="#E82900"/>
@@ -18,7 +73,8 @@
       <path d="M2.07715 16.2096V1.97803H4.74423C6.38687 1.97803 7.4643 3.00981 7.4643 4.73539V6.78118C7.4643 7.7596 7.23468 8.4356 6.52817 8.89812C7.23468 9.34286 7.49962 10.0366 7.49962 11.0151V13.4344C7.49962 15.16 6.42219 16.2096 4.76189 16.2096H2.07715ZM4.76189 14.6085C5.38009 14.6085 5.78633 14.2172 5.78633 13.47V10.8016C5.78633 9.9477 5.45074 9.6097 4.72657 9.6097H3.79044V14.6085H4.76189ZM4.72657 8.0976C5.45074 8.07981 5.75101 7.74181 5.75101 6.9057V4.69981C5.75101 3.95266 5.34477 3.57908 4.74423 3.57908H3.79044V8.0976H4.72657Z" fill="white"/>
       <path d="M123.174 19.3701L123.184 19.4659H123.189L123.19 19.4844H123.186L123.196 19.579H123.148L123.138 19.4856L116.681 19.665C116.681 19.6306 116.681 19.5974 116.681 19.563L117.161 19.5495L123.138 19.4672L123.127 19.3726H123.175L123.174 19.3701ZM9.19216 21.5716L14.1097 21.6318L16.5684 21.6613L19.0272 21.6822L23.9448 21.7227L28.8623 21.7497C32.1407 21.7719 35.4191 21.7731 38.6959 21.7866C41.9743 21.7903 45.2512 21.7952 48.5295 21.8001H54.7841L61.0402 21.7891L62.6046 21.7866L64.1676 21.7792L67.2948 21.7657L70.4221 21.751L73.5495 21.7276C81.889 21.6674 90.2299 21.5691 98.5634 21.4033C100.941 21.3566 103.318 21.305 105.694 21.251C111.565 21.1994 117.435 21.1281 123.302 21.0384L123.275 20.7804C123.307 20.7804 123.338 20.778 123.369 20.778L123.094 18.1748C123.063 18.1748 123.031 18.1748 123 18.176L122.973 17.918C115.471 18.1404 107.965 18.3173 100.457 18.4524C99.8077 18.4573 99.1586 18.4635 98.5095 18.4684C73.4372 18.6379 48.3515 18.4229 23.3047 17.8173C22.5838 17.8001 21.8614 17.7878 21.1406 17.7743L18.9749 17.7386C17.5302 17.7178 16.0869 17.6969 14.6421 17.6772L14.7439 17.9278C17.9026 18.0507 21.0687 18.1563 24.2394 18.2288C25.8247 18.2657 27.4101 18.3038 28.9984 18.3308C30.5852 18.3566 32.1721 18.3824 33.7604 18.407C40.1123 18.4942 46.4656 18.5495 52.807 18.6367L55.2059 18.6735L57.6048 18.7043L62.4027 18.7669C63.7054 18.7804 65.0096 18.7964 66.3122 18.8111C60.5228 18.8382 54.7318 18.8517 48.9423 18.8455C48.6805 18.8418 48.4188 18.8406 48.1571 18.8369L48.1541 18.8455C34.1837 18.8283 20.2147 18.7141 6.24579 18.5016C3.36077 18.4573 2.28546 18.563 2.23461 18.9106C2.12842 19.6478 1.66029 20.3787 1.63038 21.1171C1.62589 21.2252 3.07962 21.4254 4.09214 21.477C5.69094 21.5569 7.48418 21.5458 9.19515 21.5728" fill="#E82900"/>
     </svg>
-  `,c=`
+  `,
+    c = `
     <svg xmlns="http://www.w3.org/2000/svg" width="153" height="22" viewBox="0 0 153 22" fill="none">
       <path d="M146.933 15.203L147.857 2.33425H146.915V0.703003H150.177C151.99 0.703003 152.969 1.718 152.969 3.72988V15.203H150.014V13.5718H151.21V3.65738C151.21 2.75113 150.866 2.33425 150.177 2.33425H149.452L148.691 15.203H146.933Z" fill="white"/>
       <path d="M139.657 15.203V0.703003H142.449C144.261 0.703003 145.258 1.718 145.258 3.72988V15.203H143.5V3.65738C143.5 2.75113 143.156 2.33425 142.449 2.33425H141.416V15.203H139.657Z" fill="white"/>
@@ -43,7 +99,8 @@
       <path d="M7.389 15.203V2.33425H6.229V0.703003H9.14713V15.203H7.389Z" fill="#E82900"/>
       <path d="M152.723 18.4261L152.736 18.5444H152.742L152.743 18.5671H152.738L152.751 18.6839H152.691L152.678 18.5686L144.581 18.79C144.581 18.7475 144.581 18.7066 144.581 18.6642L145.183 18.6475L152.678 18.5459L152.665 18.4291H152.725L152.723 18.4261ZM9.7814 21.1434L15.9484 21.2177L19.0318 21.2541L22.1153 21.2799L28.2824 21.33L34.4493 21.3633C38.5606 21.3906 42.6719 21.3921 46.7814 21.4088C50.8927 21.4134 55.0021 21.4194 59.1135 21.4255H66.9572L74.8028 21.4118L76.7647 21.4088L78.7247 21.3997L82.6466 21.383L86.5685 21.3648L90.4904 21.336C100.949 21.2617 111.409 21.1404 121.86 20.9357C124.842 20.8781 127.822 20.8144 130.803 20.7477C138.164 20.684 145.526 20.596 152.884 20.4853L152.85 20.1669C152.89 20.1669 152.929 20.1639 152.969 20.1639L152.623 16.9506C152.584 16.9506 152.545 16.9506 152.505 16.9521L152.471 16.6337C143.063 16.9082 133.65 17.1265 124.234 17.2933C123.42 17.2994 122.606 17.307 121.792 17.313C90.3497 17.5223 58.8902 17.2569 27.4795 16.5094C26.5755 16.4881 25.6696 16.473 24.7656 16.4563L22.0497 16.4123C20.2379 16.3865 18.4279 16.3607 16.6161 16.3365L16.7437 16.6458C20.7049 16.7975 24.6755 16.9279 28.6518 17.0173C30.6399 17.0628 32.628 17.1098 34.6199 17.1432C36.6099 17.1751 38.6 17.2069 40.5919 17.2372C48.5576 17.3449 56.5251 17.4131 64.4777 17.5208L67.4862 17.5663L70.4945 17.6042L76.5115 17.6815C78.1451 17.6982 79.7807 17.7179 81.4143 17.7361C74.1539 17.7695 66.8915 17.7862 59.6311 17.7786C59.3028 17.774 58.9746 17.7725 58.6464 17.768L58.6426 17.7786C41.1226 17.7573 23.6045 17.6163 6.08643 17.354C2.4684 17.2994 1.11987 17.4298 1.0561 17.8589C0.922935 18.7688 0.335865 19.671 0.298353 20.5824C0.292726 20.7158 2.1158 20.963 3.38558 21.0267C5.3906 21.1252 7.63947 21.1116 9.78516 21.145" fill="#E82900"/>
     </svg>
-  `,d=`<svg width="294" height="174" viewBox="0 0 294 174" fill="none" xmlns="http://www.w3.org/2000/svg">
+  `,
+    d = `<svg width="294" height="174" viewBox="0 0 294 174" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M287.812 162.612L287.837 163.055H287.847L287.851 163.141H287.84L287.865 163.579H287.752L287.728 163.146L272.54 163.977C272.54 163.818 272.54 163.664 272.54 163.505L273.669 163.442L287.728 163.061L287.703 162.623H287.816L287.812 162.612ZM19.6966 172.806L31.2639 173.084L37.0476 173.221L42.8313 173.318L54.3988 173.505L65.9662 173.63C73.6778 173.733 81.3894 173.738 89.0975 173.801C96.8091 173.818 104.517 173.841 112.229 173.864H126.941L141.657 173.812L145.337 173.801L149.014 173.767L156.37 173.704L163.726 173.636L171.083 173.528C190.699 173.249 210.32 172.794 229.922 172.026C235.516 171.81 241.106 171.571 246.696 171.321C260.505 171.082 274.313 170.752 288.114 170.337L288.051 169.142C288.125 169.142 288.199 169.131 288.273 169.131L287.626 157.077C287.552 157.077 287.478 157.077 287.404 157.082L287.341 155.888C269.694 156.917 252.037 157.736 234.376 158.362C232.849 158.385 231.322 158.413 229.795 158.436C170.819 159.221 111.81 158.226 52.893 155.421C51.1973 155.342 49.4982 155.285 47.8025 155.222L42.7083 155.057C39.3099 154.96 35.9148 154.864 32.5164 154.773L32.7557 155.933C40.1858 156.502 47.6335 156.991 55.0918 157.327C58.821 157.498 62.55 157.674 66.2862 157.799C70.0189 157.918 73.7517 158.038 77.4879 158.152C92.4291 158.556 107.374 158.812 122.29 159.215L127.933 159.386L133.576 159.528L144.862 159.818C147.927 159.881 150.994 159.955 154.059 160.023C140.44 160.148 126.818 160.211 113.2 160.183C112.584 160.165 111.968 160.16 111.353 160.143L111.346 160.183C78.4834 160.103 45.6247 159.574 12.7659 158.59C5.97956 158.385 3.45013 158.874 3.33052 160.484C3.08073 163.897 1.97957 167.282 1.9092 170.701C1.89865 171.201 5.3182 172.129 7.69993 172.368C11.4607 172.737 15.6789 172.686 19.7036 172.811" fill="#E82900"/>
   <path d="M279.719 140.275V91.1399H264.425V84.9115H286.432V140.275H279.719ZM265.532 140.275V101.728H272.245V140.275H265.532Z" fill="white"/>
   <path d="M238.907 140.275V134.047H249.496V91.1399H238.907V84.9115H256.208V134.047H259.115V140.275H238.907Z" fill="white"/>
@@ -67,7 +124,14 @@
   <path d="M34.9912 68.8636V62.6352H39.5587V19.7284H36.0293V13.5H50.2162C57.1367 13.5 60.9429 17.3755 60.9429 25.0572V68.8636H54.2301V24.7803C54.2301 21.3201 52.9152 19.7284 50.2162 19.7284H46.2716V68.8636H34.9912Z" fill="white"/>
   <path d="M7.63647 68.8636V13.5H29.0207V68.8636H7.63647ZM14.3493 62.9812H22.3078V19.3824H14.3493V62.9812Z" fill="white"/>
   </svg>
-  `,n="סגור חלון צף בנושא החטופים בעזה",o="Close Floating Window about the Hostages",p="חלון צף בנושא החזרת החטופים מעזה",m="Floating Window about the Bring the Hostages Home",x=1696648800;let f;const L=`
+  `,
+    n = "סגור חלון צף בנושא החטופים בעזה",
+    o = "Close Floating Window about the Hostages",
+    p = "חלון צף בנושא החזרת החטופים מעזה",
+    m = "Floating Window about the Bring the Hostages Home",
+    x = 1696648800;
+  let f;
+  const L = `
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300;700&family=Open+Sans:wght@400;600;700;800&display=swap');
 
 #bthn {
@@ -542,34 +606,110 @@ letter-spacing: 0.69px;
 }
 
 
-`;function H(t,i){return`
+`;
+  function H(t, i) {
+    return `
     <div class="bthn-bthn-time-block">
       <div class="bthn-time">
-        ${i.toString().padStart(2,"0").split("").map(e=>`<span class="bthn-digit">${e}</span>`).join("")}      
+        ${i
+          .toString()
+          .padStart(2, "0")
+          .split("")
+          .map((e) => `<span class="bthn-digit">${e}</span>`)
+          .join("")}      
               </div>
       <div>${t}</div>
     </div>
-  `}function C(t,i,s,e,a){const b=document.getElementById("bthnTimeBlocks");if(b){const h=l;b.innerHTML=`
-        ${H(h[a].days,t)}
+  `;
+  }
+  function C(t, i, s, e, a) {
+    const b = document.getElementById("bthnTimeBlocks");
+    if (b) {
+      const h = l;
+      b.innerHTML = `
+        ${H(h[a].days, t)}
         <span class="bthn-dots">:</span>
-        ${H(h[a].hours,i)}
+        ${H(h[a].hours, i)}
         <span class="bthn-dots">:</span>
-        ${H(h[a].minutes,s)}
+        ${H(h[a].minutes, s)}
         <span class="bthn-dots">:</span>
-        ${H(h[a].seconds,e)}
-      `}}function g(){const t=new Date(x*1e3),s=new Date().getTime()-t.getTime(),e=Math.floor(s/1e3),a=Math.floor(e/86400),b=Math.floor(e%86400/3600),h=Math.floor(e%3600/60),y=e%60,V=document.querySelector("#bthn"),v=(V==null?void 0:V.getAttribute("lang"))==="he"?"he":"en";C(a,b,h,y,v)}function u(){const t=document.querySelector("#bthn"),i=t==null?void 0:t.getAttribute("lang");t==null||t.setAttribute("role","dialog"),t==null||t.setAttribute("aria-modal","false"),t==null||t.setAttribute("aria-label",i==="he"?p:m);const s=!(t!=null&&t.getAttribute("not-clickable"))&&!(t!=null&&t.classList.contains("bthn-mako")),e=i==="he"?"he":"en",b=`https://stories.bringthemhomenow.net/?utm_source=${window.location.hostname}&utm_medium=banner`;t.innerHTML=`
+        ${H(h[a].seconds, e)}
+      `;
+    }
+  }
+  function g() {
+    const t = new Date(x * 1e3),
+      s = new Date().getTime() - t.getTime(),
+      e = Math.floor(s / 1e3),
+      a = Math.floor(e / 86400),
+      b = Math.floor((e % 86400) / 3600),
+      h = Math.floor((e % 3600) / 60),
+      y = e % 60,
+      V = document.querySelector("#bthn"),
+      v = (V == null ? void 0 : V.getAttribute("lang")) === "he" ? "he" : "en";
+    C(a, b, h, y, v);
+  }
+  function u() {
+    const t = document.querySelector("#bthn"),
+      i = t == null ? void 0 : t.getAttribute("lang");
+    t == null || t.setAttribute("role", "dialog"),
+      t == null || t.setAttribute("aria-modal", "false"),
+      t == null || t.setAttribute("aria-label", i === "he" ? p : m);
+    const s =
+        !(t != null && t.getAttribute("not-clickable")) &&
+        !(t != null && t.classList.contains("bthn-mako")),
+      e = i === "he" ? "he" : "en",
+      b = `https://stories.bringthemhomenow.net/?utm_source=${window.location.hostname}&utm_medium=banner`;
+    t.innerHTML = `
       <style>${L}</style>
-      <div id="closeBthn" role="button" tabindex="0" aria-label="${e==="en"?o:n}">X</div>
-      ${s?`<a id="bthnLink" target="_blank" href="${b}">`:'<a id="bthnLink">'}
+      <div id="closeBthn" role="button" tabindex="0" aria-label="${
+        e === "en" ? o : n
+      }">X</div>
+      ${
+        s
+          ? `<a id="bthnLink" target="_blank" href="${b}">`
+          : '<a id="bthnLink">'
+      }
        
-        <div id="bthnSubtitle">${l[e].subtitle1} <span class="red-bg">${l[e].subtitle2}</span> ${l[e].subtitle3}</div>
-        <div id="bthnTimeBlocks"></div>
-        <div class="bthn-title-wrap" aria-label="${`${l[e].title1} ${l[e].title2}`}">
-          ${e==="he"?c:r}
-          
+        <div id="bthnSubtitle">${l[e].subtitle1} <span class="red-bg">${
+      l[e].subtitle2
+    }</span> ${l[e].subtitle3}</div>
+    <div id="bthnTimeBlocks"></div>
+
+    <div class="bthn-title-wrap" aria-label="${`${l[e].title1} ${l[e].title2}`}">
+          ${e === "he" ? c : r}
         </div>
         <div class="bthn-title-wrap-banner" aria-label="${`${l[e].title1} ${l[e].title2}`}">
           ${d}
         </div>
       </a>
-    `;const h=document.querySelector("#closeBthn");h&&h.addEventListener("click",w),g()}function w(t){t.preventDefault(),t.stopPropagation();const i=document.querySelector("#bthn");i&&i.remove(),clearInterval(f),sessionStorage&&sessionStorage.setItem("btnhSessionClosed","true")}function M(){let t=!1;const i=document.querySelector("#bthn");return(i==null?void 0:i.classList.contains("btnh-disable-on-mobile-param"))&&(t=new URLSearchParams(window.location.search).get("mobile")==="1"),t}function k(){return!!(sessionStorage&&sessionStorage.getItem("btnhSessionClosed"))}function Z(){M()||k()||(u(),f=setInterval(g,1e3))}Z()})();
+    `;
+    const h = document.querySelector("#closeBthn");
+    h && h.addEventListener("click", w), g();
+  }
+  function w(t) {
+    t.preventDefault(), t.stopPropagation();
+    const i = document.querySelector("#bthn");
+    i && i.remove(),
+      clearInterval(f),
+      sessionStorage && sessionStorage.setItem("btnhSessionClosed", "true");
+  }
+  function M() {
+    let t = !1;
+    const i = document.querySelector("#bthn");
+    return (
+      (i == null
+        ? void 0
+        : i.classList.contains("btnh-disable-on-mobile-param")) &&
+        (t = new URLSearchParams(window.location.search).get("mobile") === "1"),
+      t
+    );
+  }
+  function k() {
+    return !!(sessionStorage && sessionStorage.getItem("btnhSessionClosed"));
+  }
+  function Z() {
+    M() || k() || (u(), (f = setInterval(g, 1e3)));
+  }
+  Z();
+})();
--- frontend/src/components/HostagesTicker.jsx
+++ frontend/src/components/HostagesTicker.jsx
@@ -10,15 +10,27 @@ const HostagesTicker = () => {
 
       const script = document.createElement("script");
       script.type = "text/javascript";
+      // מקורי
       // script.src = "https://bringthemhomenow.net/1.1.0/hostages-ticker.js";
-      // script.src = "./hostages-ticker-custom.js";
-      // script.src = "https://drive.google.com/file/d/1o7XynajFvW1j1hLcTU_yrkzIWEHyCIk5/view?usp=sharing";
-      script.src = "https://drive.google.com/uc?id=1o7XynajFvW1j1hLcTU_yrkzIWEHyCIk5&export=download";
+      // גיטהאב שלי
+      // script.src = "https://github.com/iicoen/store/blob/master/frontend/src/components/hostages-ticker-custom.js";
+      // script.src = "https://raw.githubusercontent.com/iicoen/store/refs/heads/master/frontend/src/components/hostages-ticker-custom.js";
+      // script.src = "https://raw.githubusercontent.com/iicoen/my-website/refs/heads/main/hostages-ticker-custom.js";
+      // מקומי ניסיון
+      script.src = "./hostages-ticker-custom.js";
+      // script.src = "/hostages-ticker-custom.js?version=2";
 
-      script.setAttribute(
-        "integrity",
-        "sha384-DHuakkmS4DXvIW79Ttuqjvl95NepBRwfVGx6bmqBJVVwqsosq8hROrydHItKdsne"
-      );
+
+
+      // ניסיונות בדרייב
+      // script.src = "https://drive.google.com/file/d/1o7XynajFvW1j1hLcTU_yrkzIWEHyCIk5/view?usp=sharing";
+      // script.src = "https://drive.google.com/uc?id=1o7XynajFvW1j1hLcTU_yrkzIWEHyCIk5&export=download";
+      
+
+      // script.setAttribute(
+      //   "integrity",
+      //   "sha384-DHuakkmS4DXvIW79Ttuqjvl95NepBRwfVGx6bmqBJVVwqsosq8hROrydHItKdsne"
+      // );
       script.setAttribute("crossorigin", "anonymous");
 
       // הוספת תג הסקריפט ל־<head> בדף
